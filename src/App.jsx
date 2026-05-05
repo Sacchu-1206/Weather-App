3d0809f03fdcee4f5c8b1f645b51c8c38fb6fcc8
Sacchu-1206 <sachinyadav692003@gmail.com>
2026-05-05 23:18:57 +0530

    Added weather API integration and data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { getWeatherByCity } from "./services/weatherService";
+
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const data = await getWeatherByCity(city);
+      setWeather(data);
+    } catch (error) {
+      alert("City not found");
+    }
+  };
+
   return (
-    <div>
-      <h1>Weather App</h1>
+    <div style={{ padding: "20px" }}>
+      <h1>WeatherX Pro</h1>
+
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <button onClick={handleSearch}>Search</button>
+
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>Temp: {weather.main.temp}°C</p>
+          <p>Condition: {weather.weather[0].main}</p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/services/weatherService.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// Base URL (API ka main link)
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+// Function banaya weather fetch karne ke liye
+export const getWeatherByCity = async (city) => {
+  try {
+       console.log("API KEY:", import.meta.env.VITE_WEATHER_API_KEY);
+    const response = await axios.get(BASE_URL, {
+      params: {
+        q: city,
+        appid: import.meta.env.VITE_WEATHER_API_KEY,
+        units: "metric",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log("Error fetching weather:", error);
+    throw error;
+  }
+};
