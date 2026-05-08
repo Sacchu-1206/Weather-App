d184c969cdceb76b9020939c3a76e8d5c1572f9d
Sacchu-1206 <sachinyadav692003@gmail.com>
2026-05-08 14:35:22 +0530

    Added loading and error handling states

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,35 @@ import SearchBar from "./components/weather/SearchBar";
 function App() {
   
   const [weather, setWeather] = useState(null);
+   
+  const [loading, setLoading] = useState(false);
 
-  const handleSearch = async (city) => {
-    try {
-      const data = await getWeatherByCity(city);
-      setWeather(data);
-    } catch (error) {
-      alert("City not found");
-    }
-  };
+   const [error, setError] = useState("");
+
+ const handleSearch = async (city) => {
+
+  try {
+
+    // Loading start
+    setLoading(true);
+
+    // Old error clear
+    setError("");
+
+    const data = await getWeatherByCity(city);
+
+    setWeather(data);
+
+  } catch (error) {
+
+    setError("City not found");
+
+  } finally {
+
+    // Loading stop
+    setLoading(false);
+  }
+};
 
   return (
     <div style={{ padding: "20px" }}>
@@ -21,7 +41,11 @@ function App() {
 
      <SearchBar onSearch={handleSearch} />
 
+     {loading && <h2>Loading...</h2>}
+     {error && <p style={{ color: "yellow" }}>{error}</p>}
+     
     <WeatherCard weather={weather} />
+
     </div>
   );
 }
--- src/services/weatherService.jsx
+++ src/services/weatherService.jsx
@@ -6,7 +6,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Function banaya weather fetch karne ke liye
 export const getWeatherByCity = async (city) => {
   try {
-       console.log("API KEY:", import.meta.env.VITE_WEATHER_API_KEY);
+      // console.log("API KEY:", import.meta.env.VITE_WEATHER_API_KEY);
     const response = await axios.get(BASE_URL, {
       params: {
         q: city,
